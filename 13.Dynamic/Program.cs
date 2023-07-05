9b203638675f25a3a6824ab91de97abc083b8479
jeffersonong <erson83@hotmail.com>
2023-07-05 17:18:38 +0800

    push

--- 13.Dynamic/Program.cs
+++ 13.Dynamic/Program.cs
@@ -9,6 +9,19 @@ namespace Dynamic
         static void Main(string[] args)
         {
 
+            // dynamic will cause downstream to be dynamic
+
+            dynamic a = 10;
+            dynamic b = 5;
+            var c = a + b;
+            Console.WriteLine(c);
+
+            dynamic excelObject = "mosh";
+           // need to perform more unit test
+            excelObject.Extract();
+
+
+
 
         }
 
